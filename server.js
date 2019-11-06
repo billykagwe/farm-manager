99384bd1c533a1dd97a1ac29f16b5683a80f3352
billy <billmwas2@gmail.com>
2019-11-07 00:11:18 +0300

    init

--- server.js
+++ server.js
@@ -14,4 +14,12 @@ app.use('/api/products',require('./routes/product'))
 app.use('/api/transactions',require('./routes/transaction'))
 app.use('/api/advances',require('./routes/advance'))   
   
-       
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
